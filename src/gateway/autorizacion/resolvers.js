589ecb24f9d15f03a40d8220d00095fd98f79ab5
Brayan Leon <bdleons@unal.edu.co>
2022-10-12 22:49:51 -0500

    Adding Endpoint UserInfo on GraphQL

--- src/gateway/autorizacion/resolvers.js
+++ src/gateway/autorizacion/resolvers.js
@@ -1,22 +1,28 @@
 import { generalRequest, getRequest } from '../../utilities.js';
-import {entryPoint, port, url} from './server.js';
+import { entryPoint, port, url } from './server.js';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
 
 const resolvers = {
 	Query: {
-	},
-	Mutation: {
-		createSiamUser: (_, { siamUser }) =>
-		generalRequest(`${URL}/signUp`, 'POST', siamUser),
-		loginSiamUser: (_, { siamLogin }) =>
-		generalRequest(`${URL}/login`, 'POST', siamLogin),
 		verifyToken: (_, { jwt }) => {
 			const headers = {
 				Authorization: jwt
 			};
-			return generalRequest(`${URL}/verifyToken`, 'POST',_, headers)
+			return generalRequest(`${URL}/verifyToken`, 'GET', _, headers)
 		},
+		getUserInfo: (_, { username }) =>{
+			const body = {
+				username: username
+			};
+			return generalRequest(`${URL}/getUserInfo`, 'GET', body)
+		}
+	},
+	Mutation: {
+		createSiamUser: (_, { siamUser }) =>
+			generalRequest(`${URL}/signUp`, 'POST', siamUser),
+		loginSiamUser: (_, { siamLogin }) =>
+			generalRequest(`${URL}/login`, 'POST', siamLogin),
 	}
 };
 
--- src/gateway/autorizacion/typeDefs.js
+++ src/gateway/autorizacion/typeDefs.js
@@ -1,27 +1,20 @@
 export const authTypeDef = `
   type MessageResponse {
     message: String
+    id: Int
+    username: String
   }
   type TokenResponse {
     token: String
   }
-  type SiamUser{
-    id: ID!
+  type UserInfo {
+    id: Int
+    username: String
     name: String
     surname: String
-    username: String
-    password: String
-    birthDate: String
-    role: String
-    nationality: String
-    address: String
-    city: String
-    level: Int
-    bloodType: String
-    ethnicity: String
-    militarySituation: String
     identificationType: String
     identificationNumber: Int
+    role: String
   }
   input SiamLoginInput {
     username: String!
@@ -47,10 +40,11 @@ export const authTypeDef = `
   `;
 
 export const authQueries = `
+    verifyToken(jwt: String!): MessageResponse
+    getUserInfo(username: String!): UserInfo
   `;
 
 export const authMutations = `
     createSiamUser(siamUser: SiamUserInput): MessageResponse
     loginSiamUser(siamLogin: SiamLoginInput): TokenResponse
-    verifyToken(jwt: String!): String
 `;
