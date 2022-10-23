dc1910b22b5047d62684b64c662a013a78122ec4
Jverdugoa <56849105+Jverdugoa@users.noreply.github.com>
2022-10-22 22:05:35 -0500

    E arreglado

--- src/gateway/ingresar_calificaciones/profesor/typeDefs.js
+++ src/gateway/ingresar_calificaciones/profesor/typeDefs.js
@@ -1,28 +1,28 @@
 export const profesorinTypeDef=`
 
 type profesorin{
-    iD_Profesor : Int!
-    codigo_Asignatura : Int!
-    nombre_Profesor : String!
-    facultad_Profesor : String!
+    iD_Profesor : Int
+  codigo_Asignatura : Int
+  nombre_Profesor : String
+  facultad_Profesor : String
     
 }
 
 
 
 type profesorinID{
-    iD_Profesor : Int!
-    codigo_Asignatura : Int!
-    nombre_Profesor : String!
-    facultad_Profesor : String!
+    iD_Profesor : Int
+  codigo_Asignatura : Int
+  nombre_Profesor : String
+  facultad_Profesor : String
 }
 
 input profesorinInput{
-    iD_Profesor : Int!
-    codigo_Asignatura : Int!
-    nombre_Profesor : String!
-    facultad_Profesor : String!
-
+    iD_Profesor : Int
+    codigo_Asignatura : Int
+    nombre_Profesor : String
+    facultad_Profesor : String
+}
 `;
 
 export const profesorinMutations=`
