ef5315c1d230ee50731be12460bca086fcad8711
Jverdugoa <56849105+Jverdugoa@users.noreply.github.com>
2022-11-26 12:59:04 -0500

    Request fix

--- src/gateway/Consumo/calificaciones/server.js
+++ src/gateway/Consumo/calificaciones/server.js
@@ -1,3 +1,3 @@
-export const urlI_Ca= 'localhost'
-export const portI_Ca = '8008'
+export const urlI_Ca= 'siam_ingresar_calificaciones_ms'
+export const portI_Ca = '80'
 export const entryPointI_Ca = 'api'
